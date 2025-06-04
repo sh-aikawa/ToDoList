2b8f98a7ef6a535bbf87d96cb4b2dc5a1571ba00
sh-aikawa <aikawasho@cy-com.co.jp>
2025-06-04 14:53:14 +0900

    特になしby sh-aikawa

--- todolist/src/main/java/com/example/todolist/mapper/ToDoMapper.java
+++ todolist/src/main/java/com/example/todolist/mapper/ToDoMapper.java
@@ -2,11 +2,10 @@ package com.example.todolist.mapper;
 
 import java.util.List;
 
-import com.example.todolist.model.Task;
 import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Insert;
 
 import com.example.todolist.model.Task;
 
@@ -19,6 +18,6 @@ public interface ToDoMapper {
     @Delete("DELETE FROM tasks WHERE task_id = #{taskId}")
     void deleteTask(long taskId);
 
-    @Insert("INSERT INTO tasks(title, limit_date, description) VALUES (#{title}, #{limit_date}, #{description})")
+    @Insert("INSERT INTO tasks(title, limit_date, description) VALUES (#{title}, #{limitDate}, #{description})")
     void insertTask(Task task);
 }
