f5c75ef9146e276cc3bb46cd08c5cd48d78c5796
sh-aikawa <aikawasho@cy-com.co.jp>
2025-06-05 11:43:40 +0900

    db調整 by sh-aikawa

--- todolist/src/main/resources/schema.sql
+++ todolist/src/main/resources/schema.sql
@@ -2,8 +2,18 @@ DROP TABLE IF EXISTS tasks; /* tasksを削除し初期化*/
 
 CREATE TABLE tasks(
     task_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
     title TEXT,
     limit_date DATE,
     description TEXT,
     checked BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE users(
+    user_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
